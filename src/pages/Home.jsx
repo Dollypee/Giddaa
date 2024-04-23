ad35c4db1929ee914b6d08b416743747811c514a
Adedolapo John Olaiya <ajolaiya@sidmach.com>
2024-04-23 19:36:50 +0100

    Additional Update to Landing Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import Footer from "../components/landingpage/Footer"
+import GetStarted from "../components/landingpage/GetStarted"
 import HereIsWhy from "../components/landingpage/HereIsWhy"
 import HeroSection from "../components/landingpage/HeroSection"
+import HowItWorks from "../components/landingpage/HowItWorks"
 import MetricsSection from "../components/landingpage/MetricsSection"
 import NaijaRemoteWork from "../components/landingpage/NaijaRemoteWork"
 import NavMenu from "../components/landingpage/NavMenu"
 import PrivateSectorWork from "../components/landingpage/PrivateSectorWork"
+import Property from "../components/landingpage/Property"
 import PublicSectorWork from "../components/landingpage/PublicSectorWork"
 import PurchaseOptions from "../components/landingpage/PurchaseOptions"
 import TrustedPartners from "../components/landingpage/TrustedPartners"
@@ -50,6 +54,16 @@ const Home = () => {
 
       {/* Public Sector Section */}
       <PublicSectorWork />
+
+      {/* How it Works Section */}
+      <HowItWorks />
+
+      {/* Get started Section */}
+      <GetStarted />
+
+      {/* Footer */}
+      <Footer />
+      <Property />
 
     </div>
   )
